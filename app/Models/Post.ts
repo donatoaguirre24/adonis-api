b963ca090292f78c201421be92db3e12d8f5feab
Donato Aguirre <donatoaguirre24@gmail.com>
2020-08-07 17:02:37 -0300

    Update dependencies

--- app/Models/Post.ts
+++ app/Models/Post.ts
@@ -1,19 +1,19 @@
-import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { DateTime } from 'luxon'
 
 export default class Post extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id!: number
 
   @column()
-  public title: string
+  public title!: string
 
   @column()
-  public content: string
+  public content!: string
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt!: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt!: DateTime
 }
