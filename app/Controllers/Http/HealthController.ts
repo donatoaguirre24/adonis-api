ac853ac9e10f5f3a475d398e41142fafb8ee28dd
Donato Aguirre <donatoaguirre24@gmail.com>
2020-08-22 17:54:43 -0300

    Update HealthCheck

--- app/Controllers/Http/HealthController.ts
+++ app/Controllers/Http/HealthController.ts
@@ -4,6 +4,9 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class HealthController {
   public async index ({ response }: HttpContextContract) {
     const report = await HealthCheck.getReport()
-    return response.ok(report)
+
+    return report.healthy
+      ? response.ok(report)
+      : response.badRequest(report)
   }
 }
